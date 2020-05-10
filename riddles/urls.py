b6358588103c834824b036e65b649c248e4b9cda
TarasovaYuliya <tarasovayuliya1.19@gmail.com>
2020-05-10 15:09:50 +0300

    Практическая работа №6

--- riddles/urls.py
+++ riddles/urls.py
@@ -7,5 +7,13 @@ app_name = 'riddles'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^([0-9]+)/$', views.detail, name='detail'),
-    url(r'^([0-9]+)/answer/$', views.answer, name='answer')
+    url(r'^([0-9]+)/answer/$', views.answer, name='answer'),\
+    url(r'^register/$', views.RegisterFormView.as_view()),
+    url(r'^login/$', views.LoginFormView.as_view()),
+    url(r'^logout/$', views.LogoutView.as_view()),
+    url(r'^password-change/', views.PasswordChangeView.as_view()),
 ]
+
+
+
+
